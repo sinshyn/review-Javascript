e5836dd722c0ec9bae4b04def93d192629adfe1e
Shyn <sinshyn11@gmail.com>
2026-04-02 01:12:44 +0700

    operator + data type

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// var fullName = "Toan Sang";
+// var age = 26;
+// alert("hello " + fullName + age);
+// console.clear(fullName);
+// console.count(age + 6);
+// confirm("xac nhan do tuoi");
+// prompt("xac nhan do tuoi");
+// setTimeout(function () {
+//   console.log(fullName);
+// }, 1000);
+// setInterval(function () {
+//   console.count(fullName);
+// }, 3000);
+var a = 1;
+var b = 2;
+
+var userName = "Sang";
+userName += " Sang";
+console.log(userName);
+
+if (a <= b) {
+  console.log("dieu kien dung");
+} else {
+  console.log("dieu kien sai");
+}
